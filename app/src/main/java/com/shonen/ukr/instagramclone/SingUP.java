d6f2d803fac15f71f2e12b65e35d9dc7dfb4d745
Dima Usatyuk <Shonen1988@gmail.com>
2020-03-30 17:00:10 +0300

    practice to save object to parse server( updated), add FancyToast library

--- app/src/main/java/com/shonen/ukr/instagramclone/SingUP.java
+++ app/src/main/java/com/shonen/ukr/instagramclone/SingUP.java
@@ -4,16 +4,19 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
 import com.parse.ParseException;
 import com.parse.ParseObject;
 import com.parse.SaveCallback;
+import com.shashank.sony.fancytoastlib.FancyToast;
 
 public class SingUP extends AppCompatActivity {
 
     private EditText edtName, edtPunchPower, edtPunchSpeed, edtKickPower, edtKickSpeed;
+    private Button saveBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,34 +28,36 @@ public class SingUP extends AppCompatActivity {
         edtPunchSpeed = findViewById(R.id.edtPunchSpeed);
         edtKickPower = findViewById(R.id.edtKickPower);
         edtKickSpeed = findViewById(R.id.edtKickSpeed);
-    }
+        saveBtn = findViewById(R.id.saveBtn);
 
-    public void helloWordTapped(View view) {
-//        ParseObject boxer = new ParseObject("Boxer");
-//        boxer.put("punchValue", 200);
-//        boxer.saveInBackground(new SaveCallback() {
-//            @Override
-//            public void done(ParseException e) {
-//                if (e == null) {
-//                    Toast.makeText(SingUP.this,"boxer object was saved",Toast.LENGTH_LONG).show();
-//                }
-//            }
-//        });
-
-        ParseObject kickBoxer = new ParseObject("KickBoxer");
-        kickBoxer.put("name",edtName.getText().toString());
-        kickBoxer.put("punchPower", Integer.parseInt(edtPunchPower.getText().toString()));
-        kickBoxer.put("punchSpeed", Integer.parseInt(edtPunchSpeed.getText().toString()));
-        kickBoxer.put("kickPower",Integer.parseInt(edtKickPower.getText().toString()));
-        kickBoxer.put("kickSpeed", Integer.parseInt(edtKickSpeed.getText().toString()));
-        kickBoxer.saveInBackground(new SaveCallback() {
+        saveBtn.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void done(ParseException e) {
-                if (e == null) {
-                    Toast.makeText(SingUP.this,"kickboxer object was saved",Toast.LENGTH_LONG).show();
+            public void onClick(View v) {
+
+
+                try {
+                    ParseObject kickBoxer = new ParseObject("KickBoxer");
+                    kickBoxer.put("name", edtName.getText().toString());
+                    kickBoxer.put("punchPower", Integer.parseInt(edtPunchPower.getText().toString()));
+                    kickBoxer.put("punchSpeed", Integer.parseInt(edtPunchSpeed.getText().toString()));
+                    kickBoxer.put("kickPower", Integer.parseInt(edtKickPower.getText().toString()));
+                    kickBoxer.put("kickSpeed", Integer.parseInt(edtKickSpeed.getText().toString()));
+                    kickBoxer.saveInBackground(new SaveCallback() {
+                        @Override
+                        public void done(ParseException e) {
+                            if (e == null) {
+                                FancyToast.makeText(SingUP.this, "kickboxer object was saved", FancyToast.LENGTH_LONG, FancyToast.INFO, true).show();
+                            } else {
+                                FancyToast.makeText(SingUP.this, e.getMessage(), FancyToast.LENGTH_LONG, FancyToast.ERROR, true).show();
+                            }
+                        }
+                    });
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    FancyToast.makeText(SingUP.this, e.getMessage(), FancyToast.LENGTH_LONG, FancyToast.ERROR, true).show();
+
                 }
             }
         });
-
     }
 }
