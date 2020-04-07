33fcd8fbaab1fb2e978c084a44a3dab8156c000f
Dima Usatyuk <Shonen1988@gmail.com>
2020-04-07 11:27:55 +0300

    add logic to loig in activity

--- app/src/main/java/com/shonen/ukr/instagramclone/LogIn.java
+++ app/src/main/java/com/shonen/ukr/instagramclone/LogIn.java
@@ -2,13 +2,64 @@ package com.shonen.ukr.instagramclone;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.parse.LogInCallback;
+import com.parse.ParseException;
+import com.parse.ParseUser;
+import com.shashank.sony.fancytoastlib.FancyToast;
 
 public class LogIn extends AppCompatActivity {
+
+    private EditText edtLogInEmail, edtLogInPassword;
+    private Button btnLogIn, btnSingUp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_in);
+        edtLogInEmail = findViewById(R.id.edtLogInEmail);
+        edtLogInPassword = findViewById(R.id.edtLogInPassword);
+
+        btnLogIn = findViewById(R.id.btnLogInInActivity);
+        btnSingUp = findViewById(R.id.btnSinUpFromLO);
+
+        if (ParseUser.getCurrentUser() != null) {
+            ParseUser.getCurrentUser().logOut();
+        }
+
+        btnLogIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    ParseUser.logInInBackground(edtLogInEmail.getText().toString(), edtLogInPassword.getText().toString(), new LogInCallback() {
+                        @Override
+                        public void done(ParseUser user, ParseException e) {
+                            if (user != null && e == null) {
+                                FancyToast.makeText(LogIn.this, user.getUsername() + " was log in", Toast.LENGTH_SHORT, FancyToast.SUCCESS, false).show();
+                            } else {
+                                FancyToast.makeText(LogIn.this, "Incorrect username or password, please try again", FancyToast.LENGTH_LONG, FancyToast.ERROR, false).show();
+                            }
+
+                        }
+                    });
+                } catch (Exception e) {
+                    FancyToast.makeText(LogIn.this, "Login or password is wrong, please try again" + e.toString(), FancyToast.LENGTH_LONG, FancyToast.ERROR, false).show();
+                }
+            }
+        });
+        btnSingUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LogIn.this, SingUP.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
--- app/src/main/java/com/shonen/ukr/instagramclone/SingUP.java
+++ app/src/main/java/com/shonen/ukr/instagramclone/SingUP.java
@@ -2,6 +2,7 @@ package com.shonen.ukr.instagramclone;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -32,11 +33,14 @@ public class SingUP extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sing_up);
         edtSingUpEmail = findViewById(R.id.edtSingUpEmail);
-        edtSingUpUsername=findViewById(R.id.edtSingUpUserName);
-        edtSingUpPassword=findViewById(R.id.edtSingUpPassword);
+        edtSingUpUsername = findViewById(R.id.edtSingUpUserName);
+        edtSingUpPassword = findViewById(R.id.edtSingUpPassword);
 
-        btnSingUp=findViewById(R.id.btnSingUp);
-        btnLogIn=findViewById(R.id.btnLogIn);
+        btnSingUp = findViewById(R.id.btnSingUp);
+        btnLogIn = findViewById(R.id.btnLogIn);
+        if (ParseUser.getCurrentUser() != null) {
+            ParseUser.getCurrentUser().logOut();
+        }
 
         btnSingUp.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -46,27 +50,32 @@ public class SingUP extends AppCompatActivity {
                     addNewUser.setEmail(edtSingUpEmail.getText().toString());
                     addNewUser.setUsername(edtSingUpUsername.getText().toString());
                     addNewUser.setPassword(edtSingUpPassword.getText().toString());
+                    final ProgressDialog progressDialog = new ProgressDialog(SingUP.this);
+                    progressDialog.setMessage("Singing up " + edtSingUpUsername.getText().toString());
+                    progressDialog.show();
                     addNewUser.signUpInBackground(new SignUpCallback() {
                         @Override
                         public void done(ParseException e) {
-                            if(e ==null){
-                                FancyToast.makeText(SingUP.this,addNewUser.getUsername()+" was added", Toast.LENGTH_SHORT,FancyToast.SUCCESS,false).show();
-                            }else {
+                            if (e == null) {
+                                FancyToast.makeText(SingUP.this, addNewUser.getUsername() + " was added", Toast.LENGTH_SHORT, FancyToast.SUCCESS, false).show();
+                            } else {
                                 FancyToast.makeText(SingUP.this, e.getMessage(), FancyToast.LENGTH_LONG, FancyToast.ERROR, false).show();
                             }
+                            progressDialog.dismiss();
                         }
                     });
                 } catch (Exception e) {
-                    FancyToast.makeText(SingUP.this, e.getMessage(), FancyToast.LENGTH_LONG, FancyToast.ERROR, false).show();
+                    FancyToast.makeText(SingUP.this, "Please try again", FancyToast.LENGTH_LONG, FancyToast.ERROR, false).show();
                 }
+
             }
         });
-            btnLogIn.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    Intent intent = new Intent(SingUP.this,LogIn.class);
-                    startActivity(intent);
-                }
-            });
+        btnLogIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SingUP.this, LogIn.class);
+                startActivity(intent);
+            }
+        });
     }
 }
